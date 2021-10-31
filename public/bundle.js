f96107d70bc0c47a1e1bb60343564bdc9c84212b
DaBeatLEJ <dabeat1@hotmail.com>
2021-10-31 21:36:05 +0000

    LocalStream Viewing

--- public/bundle.js
+++ public/bundle.js
@@ -4344,13 +4344,58 @@ module.exports = yeast;
 
 },{}],37:[function(require,module,exports){
 console.log("Testing LEJ");
+
 const io = require('socket.io-client');
 
 const socket = io("/mediasoup");
 
 socket.on('connection-success', ({ socketId }) => {
     console.log('client socket = ',socketId)
-  });
+});
+
+let params = {
+    //mediasoup params
+};
+
+const getLocalStream = () => {
+    console.log('butn cl ')
+    navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        width: {
+          min: 640,
+          max: 1920,
+        },
+        height: {
+          min: 400,
+          max: 1080,
+        }
+      }
+    }).then(streamSuccess).catch(error => {
+      console.log(error.message)
+    });
+};
+
+const streamSuccess = async (stream) => {
+    console.log('stream = ',stream)
+    localVideo.srcObject = stream
+    const track = stream.getVideoTracks()[0]
+    params = {
+      track,
+      ...params
+    };    
+};
+
+btnLocalVideo.addEventListener('click', getLocalStream);
+// btnRtpCapabilities.addEventListener('click', getRtpCapabilities)
+// btnDevice.addEventListener('click', createDevice)
+// btnCreateSendTransport.addEventListener('click', createSendTransport)
+// btnConnectSendTransport.addEventListener('click', connectSendTransport)
+// btnRecvSendTransport.addEventListener('click', createRecvTransport)
+// btnConnectRecvTransport.addEventListener('click', connectRecvTransport)
+
+
+
 },{"socket.io-client":25}],38:[function(require,module,exports){
 'use strict'
 
